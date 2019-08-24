9eb568d1ac5bdbffd6e2fb30ea8acddc12bba33f
geraldprambudi <geraldprambudi94@gmail.com>
2019-08-24 21:32:10 +0700

    crud ionic 4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  taskList = [];
+  taskName: string = "";
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public alertController: AlertController) { }
 
+
+  addTask() {
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = "";
+    }
+  }
+
+  async updateTask(index) {
+    let alert = await this.alertController.create({
+      header: 'Are you sure ? ',
+      // subHeader: 'Subtitle',
+      message: 'Type in your new task to update.',
+      inputs: [{ name: 'editTask', placeholder: 'edit data' }],
+      buttons: [{ text: 'Cancel', role: 'cancel' },
+      {
+        text: 'Update', handler: data => {
+          this.taskList[index] = data.editTask;
+        }
+      }
+      ]
+    });
+    alert.present();
+  }
+
+  deleteTask(index) {
+    this.taskList.splice(index, 1);
+  }
 }
